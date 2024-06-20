676a2391246baf2008ce2d5bdbc4059233e112ad
unknown <primusr@M303.UC-BCF.EDU.PH>
2024-06-20 10:57:37 +0800

    Added button to homepage, it redirects to products page

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
